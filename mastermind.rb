bc57ef2084ef184d9538ad9d3a3c495fb691988b
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-01 14:58:31 -0400

    Added fuctionality

--- mastermind.rb
+++ mastermind.rb
@@ -3,14 +3,15 @@ class MasterMind
   # initialize game
   def initialize
     @board = Board.new
+    @code = []
   end
 
   def set_players
-    @player_one = HumanPlayer.new("codebreaker")
+    @player_one = HumanPlayer.new("codebreaker", "Player 1")
     if check_players == "h"
-      @player_two = HumanPlayer.new("codesetter")
+      @player_two = HumanPlayer.new("codesetter", "Player 2")
     else
-      @player_two = ComputerPlayer.new("codesetter")
+      @player_two = ComputerPlayer.new("codesetter", "SirBeepington")
     end
   end
 
@@ -21,6 +22,21 @@ class MasterMind
     reponse
   end
   # run game loop
+  def choose_code
+    valid = false
+    while valid = false
+      @code = @player_one.set_code || @player_two.set_code
+      if @code.code_valid? 
+        valid = true
+      else 
+        puts "that was not a valid code, try again"
+      end
+    end
+  end
+
+  def game_loop
+    choose_code
+  end
   # check game over
   # check win
   # check loss
@@ -28,6 +44,16 @@ class MasterMind
   # play again?
   # reverse player roles
   # quit
+
+  def code_valid?(code)
+    valid = true
+    code.each.to_i do |number|
+      if number > 6 || number < 1
+        valid = false 
+      end
+    end
+    valid
+  end
 end
 
 class Board
@@ -37,7 +63,7 @@ class Board
     PEG_COLOR = %w(1 2 3 4 5 6)
     @guesses = []
     @results = []
-  # code is set
+  end
   # prompt guesser for move
   # checks guess
   # render board
@@ -45,12 +71,28 @@ class Board
 end
 
 class HumanPlayer
-  # player decides roles
-  # sets code
+  def initialize(role = "codesetter")
+    @role = role
+    @name = name
+  end
+  def set_code
+    if role = "codesetter"
+      puts "You get to set the code, #{@name}"
+      puts "Pick four numbers between 1 and 6 inclusive"
+      puts "In the following format '1 2 3 4' "
+      code = gets.chomp.split("")
+    else
+      code = nil
+    end
+    code  
+  end
   # make guess
 end
 
 class ComputerPlayer
+  def initialize(role = "codesetter")
+    @role = role
+  end
   # determines code
   # sets code
   # algorithm
