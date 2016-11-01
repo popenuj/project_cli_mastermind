89719d7c469941da1ffe2bfa76174aa354b376f8
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-01 14:07:49 -0400

    started mastermind

---
+++ mastermind.rb
@@ -0,0 +1,35 @@
+class MasterMind
+
+end
+
+class Board
+
+end
+
+class HumanPlayer
+
+end
+
+class ComputerPlayer
+
+end
+
+#start of game 
+
+#Optional: decide number of games
+
+#player roles are assigned
+#codesetter sets code (6 colors, 4 pegs)
+
+#start game loop, till 12 attempts
+#codebreaker guesses four colors
+#board returns which guesses are correct
+#and how many colors are correct
+# 1 perfect, 2 color right, 1 wrong
+#if 4 perfect break loop, declare winner. 
+
+# use <=> for perfect, color right, or wrong
+
+#do you want to play again? 
+#if yes, switch sides, play again. 
+
